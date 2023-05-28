3d8359aff33811bd8dd1b399751967a3cd38105b
Julian Stradolini <julianstradolini@gmail.com>
2023-05-28 16:33:50 -0300

    js for bs plugins

---
+++ src/app.js
@@ -0,0 +1,3 @@
+//para activar pop overs de bootstrap //
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
